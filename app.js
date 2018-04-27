4bcdd033997234f8d81c125cd938b5096288deb4
Gray Smith <gray.smith00@gmail.com>
2018-04-27 13:41:36 -0600

    clean up and reformat code

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const ejs = require('ejs');
-const axios = require('axios');
+const ejs = require("ejs");
+const axios = require("axios");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => {
-  
-  res.render('index');
-  
-  
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 module.exports = app;
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,85 +1,112 @@
-const table = document.querySelector('#leaderboardTable');
-const playerBio = document.querySelector('#player-bio');
-const playerSearchInput = document.querySelector('#playerSearchInput');
-const playerSearchSubmit = document.querySelector('#playerSearchSubmit');
-const searchPlayerAlert = document.querySelector('#search-player-alert');
-const favoritePlayer = document.querySelector('#favorite-player');
+const leaderboardTable = document.querySelector("#leaderboardTable");
+const playerBio = document.querySelector("#player-bio");
+const playerSearchInput = document.querySelector("#playerSearchInput");
+const playerInfoButton = document.querySelector("#playerInfoButton");
+const playerSearchSubmit = document.querySelector("#playerSearchSubmit");
+const searchPlayerAlert = document.querySelector("#search-player-alert");
+const favoritePlayer = document.querySelector("#favorite-player");
 
-
-function getLeaderboardData () {
-  axios.get('https://statdata.pgatour.com/r/014/leaderboard-v2mini.json')
-  .then(function(res) {
-    const playerData = res.data.leaderboard.players.map(player => {
-      return `
+function getLeaderboardData() {
+  axios
+    .get("https://statdata.pgatour.com/r/014/leaderboard-v2mini.json")
+    .then(function(res) {
+      const playerData = res.data.leaderboard.players
+        .map(player => {
+          return `
         <div class="row">
           <div class="cell" data-title="position" id="position">
           ${player.current_position}
           </div>
           <div class="cell" data-title="name" id="name">
-          ${player.player_bio.last_name}
+          ${player.player_bio.short_name} ${player.player_bio.last_name}
           </div> 
           <div class="cell" data-title="currentRoundScore" id="currentRoundScore">
-            ${player.today && player.today < 0 ? `${player.today}` : (player.today > 0 ? `+${player.today}` : (player.today === 0 ? 'E' : (player.rounds[player.rounds.length-1].tee_time ? `${player.rounds[player.rounds.length-1].tee_time}` : 'Missed Cut')))}
+            ${
+              player.today && player.today < 0
+                ? `${player.today}`
+                : player.today > 0
+                  ? `+${player.today}`
+                  : player.today === 0
+                    ? "E"
+                    : player.rounds[player.rounds.length - 1].tee_time
+                      ? `${player.rounds[player.rounds.length - 1].tee_time}`
+                      : "Missed Cut"
+            }
           </div>
           <div class="cell" data-title="thru" id="thru">
-            ${player.thru ? `${player.thru}` : '-'}
+            ${player.thru ? `${player.thru}` : "-"}
           </div>
           <div class="cell" data-title="totalScore" id="totalScore">
-            ${player.total === 0 ? 'E' : (player.total > 0 ? `+${player.total}` : `${player.total}`)}
+            ${
+              player.total === 0
+                ? "E"
+                : player.total > 0
+                  ? `+${player.total}`
+                  : `${player.total}`
+            }
           </div>
         </div>
-      `;  
-    }).join('');
+      `;
+        })
+        .join("");
 
-    table.innerHTML = 
-    `<div class="row header">
-      <div class="cell">
-        Pos
+      leaderboardTable.innerHTML = `
+      <div class="row header">
+        <div class="cell">
+          Pos
+        </div>
+        <div class="cell">
+          Player
+        </div>
+        <div class="cell">
+          Round ${res.data.leaderboard.current_round}
+        </div>
+        <div class="cell">
+          Thru
+        </div>
+        <div class="cell">
+          Total
+        </div>
       </div>
-      <div class="cell">
-        Player
-      </div>
-      <div class="cell">
-        Round ${res.data.leaderboard.current_round}
-      </div>
-      <div class="cell">
-        Thru
-      </div>
-      <div class="cell">
-        Total
-      </div>
-    </div>
-    ${playerData}`;
-  })
-  .catch(function(error) {
-    console.log(error);
-  })
-}  
+      ${playerData}
+    `;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
 getLeaderboardData();
 
-setInterval(function(){  
+setInterval(function() {
   getLeaderboardData();
-}, 600000);
-
-function winnerTrophy() {
-
-}
+}, 6000000);
 
 function getTopPlayers() {
-  axios.get('https://statdata.pgatour.com/r/014/leaderboard-v2mini.json')
-  .then(function(res) {
-    const topPlayerData = res.data.leaderboard.players.slice(0,10).map(player => {
-      return `
+  axios
+    .get("https://statdata.pgatour.com/r/014/leaderboard-v2mini.json")
+    .then(function(res) {
+      const topPlayerData = res.data.leaderboard.players
+        .slice(0, 10)
+        .map(player => {
+          return `
         <div class="player-info">
           <div class="player-photo">
-            <img src="images/players/${player.player_bio.first_name}_${player.player_bio.last_name}.png" />
+            <img src="images/players/${player.player_bio.first_name}_${
+            player.player_bio.last_name
+          }.png" />
           </div>
           <div class="player-info-text">
             <h2>
               ${player.player_bio.first_name} ${player.player_bio.last_name}
-              <span>${player.current_position === `1` ? `<i class="fas fa-trophy"></i> 2018 Masters Champion` : ``}</span>
+              <span>${
+                player.current_position === `1`
+                  ? `<i class="fas fa-trophy"></i> 2018 Masters Champion`
+                  : ``
+              }</span>
             </h2>
-            <p class="country"><img src="images/flags/${player.player_bio.country}.png" /> <span>${player.player_bio.country}</span></p>
+            <p class="country"><img src="images/flags/${
+              player.player_bio.country
+            }.png" /> <span>${player.player_bio.country}</span></p>
             <div class="player-info-table">
               <div class="row header">
                 <div class="cell">
@@ -110,57 +137,77 @@ function getTopPlayers() {
                 </div>
               </div>
             </div>
-            <p class="player-info-total"><span>Total Score: </span>${player.total < 0 ? `${player.total}` : (player.total === 0 ? `E` : `+${player.total}`) }</p>
+            <p class="player-info-total"><span>Total Score: </span>${
+              player.total < 0
+                ? `${player.total}`
+                : player.total === 0
+                  ? `E`
+                  : `+${player.total}`
+            }</p>
           </div> 
         </div>
-      `   
-    }).join('');
-    playerBio.innerHTML = topPlayerData;
-    
-  })
-  .catch(function(error) {
-    console.log(error);
-  })
+      `;
+        })
+        .join("");
+      playerBio.innerHTML = topPlayerData;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 }
 getTopPlayers();
 
-const playerInfoButton = document.querySelector('#playerInfoButton');
-playerInfoButton.addEventListener('click', function() {
+playerInfoButton.addEventListener("click", function() {
   searchPlayerAlert.innerHTML = ``;
   playerBio.classList.toggle("display-none");
   favoritePlayer.classList.add("display-none");
 });
 
 function getFavoritePlayer() {
-  playerSearchSubmit.addEventListener('click', function(e) {
+  playerSearchSubmit.addEventListener("click", function(e) {
     e.preventDefault();
     playerBio.classList.add("display-none");
     favoritePlayer.classList.remove("display-none");
     const playerSearchValue = playerSearchInput.value;
-    axios.get('https://statdata.pgatour.com/r/014/leaderboard-v2mini.json')
+    axios
+      .get("https://statdata.pgatour.com/r/014/leaderboard-v2mini.json")
       .then(function(res) {
-        const playerData = res.data.leaderboard.players.filter(function(player) {
-          const playerFullName = `${player.player_bio.first_name} ${player.player_bio.last_name}`;
-          return playerFullName.toLowerCase() === playerSearchValue.toLowerCase(); 
+        const playerData = res.data.leaderboard.players.filter(function(
+          player
+        ) {
+          const playerFullName = `${player.player_bio.first_name} ${
+            player.player_bio.last_name
+          }`;
+          return (
+            playerFullName.toLowerCase() === playerSearchValue.toLowerCase()
+          );
         });
         const player = playerData[0];
         if (!player) {
           searchPlayerAlert.innerHTML = `<p>Ooooops! Don't recognize that player, try again!<p>`;
-          console.log('that player doesn\'t exist broo');
+          console.log("that player doesn't exist broo");
           return; // if there is no matching player stop the funciton
         }
         searchPlayerAlert.innerHTML = ``;
         const playerHTML = `
         <div class="player-info">
           <div class="player-photo">
-            <img src="images/players/${player.player_bio.first_name}_${player.player_bio.last_name}.png" />
+            <img src="images/players/${player.player_bio.first_name}_${
+          player.player_bio.last_name
+        }.png" />
           </div>
           <div class="player-info-text">
             <h2>
               ${player.player_bio.first_name} ${player.player_bio.last_name}
-              <span>${player.current_position === `1` ? `<i class="fas fa-trophy"></i> 2018 Masters Champion` : ``}</span>
+              <span>${
+                player.current_position === `1`
+                  ? `<i class="fas fa-trophy"></i> 2018 Masters Champion`
+                  : ``
+              }</span>
             </h2>
-            <p class="country"><img src="images/flags/${player.player_bio.country}.png" /> <span>${player.player_bio.country}</span></p>
+            <p class="country"><img src="images/flags/${
+              player.player_bio.country
+            }.png" /> <span>${player.player_bio.country}</span></p>
             <div class="player-info-table">
               <div class="row header">
                 <div class="cell">
@@ -191,23 +238,21 @@ function getFavoritePlayer() {
                 </div>
               </div>
             </div>
-            <p class="player-info-total"><span>Total Score: </span>${player.total < 0 ? `${player.total}` : (player.total === 0 ? `E` : `+${player.total}`) }</p>
+            <p class="player-info-total"><span>Total Score: </span>${
+              player.total < 0
+                ? `${player.total}`
+                : player.total === 0
+                  ? `E`
+                  : `+${player.total}`
+            }</p>
           </div> 
         </div>
-      `   
-      favoritePlayer.innerHTML = playerHTML; 
+      `;
+        favoritePlayer.innerHTML = playerHTML;
       })
       .catch(function(error) {
         console.log(error);
-      })
-  })
+      });
+  });
 }
 getFavoritePlayer();
-
-
-
-
-
-
-
-
